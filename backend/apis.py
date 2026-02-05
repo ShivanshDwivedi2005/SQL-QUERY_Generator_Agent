f90f37add719e31022db7063b3103f5a3a2a8b50
ShivanshDwivedi2005 <shivanshbharadwaj2004@gmail.com>
2026-02-05 07:03:33 +0530

    apis.py changed

--- backend/apis.py
+++ backend/apis.py
@@ -308,6 +308,6 @@ def health_check():
 # port = int(os.environ.get("PORT", 8000))
 # uvicorn.run(app, host="0.0.0.0", port=port)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8000))
+#     uvicorn.run(app, host="0.0.0.0", port=port)
