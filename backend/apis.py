c3f4d8238dd51615bdd27d65134480d7d806b53b
ShivanshDwivedi2005 <shivanshbharadwaj2004@gmail.com>
2026-02-05 06:28:27 +0530

    Comment out port and uvicorn run commands
    Comment out the server run commands for uvicorn.

--- backend/apis.py
+++ backend/apis.py
@@ -305,5 +305,5 @@ def health_check():
     }
 
 
-port = int(os.environ.get("PORT", 8000))
-uvicorn.run(app, host="0.0.0.0", port=port)
+# port = int(os.environ.get("PORT", 8000))
+# uvicorn.run(app, host="0.0.0.0", port=port)
